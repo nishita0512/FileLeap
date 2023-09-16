720501b3b23da9e4da9f1ece90eb1644e4023349
nishita0512 <nishitalotwani@gmail.com>
2023-09-16 16:57:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fileleap/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.fileleap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.fileleap.ui.theme.FileLeapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    FileLeapTheme {
+        Greeting("Android")
+    }
+
+}
---
+++ app/src/main/java/com/example/fileleap/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.example.fileleap.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF00D1FF)
+val Secondary = Color(0xFF2C2C2C)
