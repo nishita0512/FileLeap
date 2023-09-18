9ed67daa3ecacd9065f77031efee0f6c3b6a637e
Vinesh05 <vineshchtr@gmail.com>
2023-09-18 08:30:11 +0530

    Home Page UI Implemented

--- app/src/main/java/com/example/fileleap/MainActivity.kt
+++ app/src/main/java/com/example/fileleap/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.fileleap
 import android.annotation.SuppressLint
 import android.graphics.Paint.Style
 import android.os.Bundle
+import android.widget.Toast
 import com.example.fileleap.ui.theme.Primary
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
@@ -19,20 +25,29 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
@@ -41,125 +56,293 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fileleap.ui.theme.FileLeapTheme
+import com.example.fileleap.ui.theme.Secondary
+import com.example.fileleap.ui.theme.Typography
 import kotlinx.coroutines.launch
-
 
 data class NavigationItem(
     val title: String,
 )
 
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FileLeapTheme{
-                val items = listOf(
-                    NavigationItem(
-                        title = "How it works?"
-                    ),
-                    NavigationItem(
-                        title = "About Us"
-                    ),
-                    NavigationItem(
-                        title = "FAQ"
-                    ),
-                    NavigationItem(
-                        title = "Privacy Policy"
-                    ),
-                )
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Black
-                ){
-                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                    val scope = rememberCoroutineScope()
-                    var selectedItemIndex by rememberSaveable {
-                            mutableStateOf(0)
-                    }
-
-                    ModalNavigationDrawer(
-                        drawerContent = {
-                            ModalDrawerSheet{
-                                Spacer(modifier = Modifier.height(16.dp))
-                                items.forEachIndexed{index, item ->
-                                    NavigationDrawerItem(
-                                        label = {
-                                            Text(text = item.title)
-                                        },
-                                        selected = index == selectedItemIndex,
-                                        onClick = {
-                                            selectedItemIndex = index
-                                            scope.launch{
-                                                drawerState.close()
-                                            }
-                                        },
-                                        modifier = Modifier
-                                            .padding(NavigationDrawerItemDefaults.ItemPadding),
-
-                                        colors = NavigationDrawerItemDefaults.colors(
-                                            selectedContainerColor = Primary
-                                        )
-                                    )
-                                }
-                            }
-                        },
-                        drawerState = drawerState
-                    ) {
-                        Scaffold(
-                            topBar = {
-                                TopAppBar(
-                                    title = {
-                                        Text(text = "File Leap",
-                                            modifier = Modifier.fillMaxWidth().padding(end = 16.dp),
-                                            textAlign = TextAlign.Center,
-                                            color = Primary
-                                        )
-                                    },
-                                    navigationIcon = {
-                                        IconButton(onClick = {
-                                            scope.launch{
-                                                drawerState.open()
-                                            }
-                                        }) {
-                                            Icon(
-                                                imageVector = Icons.Default.Menu,
-                                                contentDescription = null
-                                            )
-                                        }
-                                    }
-                                )
-                            }
-                        ){
-                        }
-                    }
-                }
+                MainScreen()
             }
         }
     }
 }
 
-/*@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(){
+    val items = listOf(
+        NavigationItem(
+            title = "Home"
+        ),
+        NavigationItem(
+            title = "How it works?"
+        ),
+        NavigationItem(
+            title = "About Us"
+        ),
+        NavigationItem(
+            title = "FAQ"
+        ),
+        NavigationItem(
+            title = "Privacy Policy"
+        ),
     )
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Black
+    ){
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        var selectedItemIndex by rememberSaveable {
+            mutableStateOf(0)
+        }
+
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet{
+                    Spacer(modifier = Modifier.height(16.dp))
+                    items.forEachIndexed{index, item ->
+                        NavigationDrawerItem(
+                            label = {
+                                Text(
+                                    text = item.title,
+                                    style = Typography.bodyLarge
+                                )
+                            },
+                            selected = index == selectedItemIndex,
+                            onClick = {
+                                selectedItemIndex = index
+                                scope.launch{
+                                    drawerState.close()
+                                }
+                            },
+                            modifier = Modifier
+                                .padding(NavigationDrawerItemDefaults.ItemPadding),
+
+                            colors = NavigationDrawerItemDefaults.colors(
+                                selectedContainerColor = Primary
+                            )
+                        )
+                    }
+                }
+            },
+            drawerState = drawerState
+        ) {
+            Scaffold(
+                containerColor = Color.Black,
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = "File Leap",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(end = 16.dp),
+                                textAlign = TextAlign.Center,
+                                color = Primary,
+                                style = Typography.titleLarge
+                            )
+                        },
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+                                    scope.launch{
+                                        drawerState.open()
+                                    }
+                                },
+                                colors = IconButtonDefaults.iconButtonColors(
+                                    contentColor = Primary
+                                )
+                            ){
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = null
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.mediumTopAppBarColors(
+                            containerColor = Color.Black
+                        )
+                    )
+                }
+            ){ scaffoldPadding ->
+                when(selectedItemIndex){
+                    0->{
+                        HomePage(scaffoldPadding = scaffoldPadding)
+                    }
+                    else->{
+
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomePage(scaffoldPadding: PaddingValues){
+    Column(
+        modifier = Modifier
+            .padding(scaffoldPadding)
+            .padding(48.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Column(
+            modifier = Modifier
+                .padding(vertical = 32.dp)
+                .fillMaxWidth()
+        ){
+            Text(
+                text = "Simplified file sharing, speed and security redefined.",
+                style = Typography.titleLarge,
+                color = Primary,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "Send files of any size directly from your device without ever storing anything online.",
+                style = Typography.bodyLarge,
+                color = Color.White,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier.padding(vertical = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Column(
+                modifier = Modifier
+                    .weight(4f)
+                    .fillMaxWidth()
+                    .background(Secondary, RoundedCornerShape(16.dp))
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_add),
+                    contentDescription = "Add File",
+                    contentScale = ContentScale.Inside,
+                    modifier = Modifier
+                        .weight(5f)
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "Select  File to share",
+                    style = Typography.titleMedium,
+                    color = Color.White,
+                    modifier = Modifier
+                        .weight(3f)
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "up to 10GB",
+                    style = Typography.bodyMedium,
+                    color = Color.White,
+                    modifier = Modifier
+                        .weight(2f)
+                        .padding(4.dp)
+                )
+
+            }
+            Text(
+                text = "OR",
+                style = Typography.titleLarge,
+                color = Color.White,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .weight(4f)
+                    .fillMaxWidth()
+                    .background(Secondary, RoundedCornerShape(16.dp))
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                var code by rememberSaveable {
+                    mutableStateOf("")
+                }
+                val context = LocalContext.current
+                val maxChar = 6
+                Text(
+                    text = "Enter Receiving Code",
+                    style = Typography.titleMedium,
+                    color = Color.White,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(vertical = 4.dp)
+                )
+                OutlinedTextField(
+                    value = code,
+                    onValueChange = {
+                        if (it.length <= maxChar) code = it
+                    },
+                    maxLines = 1,
+                    textStyle = Typography.bodyMedium,
+                    modifier = Modifier
+                        .weight(2f)
+                        .padding(horizontal = 16.dp, vertical = 12.dp),
+                    supportingText = {
+                        Text(
+                            text = "${code.length} / $maxChar",
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.End,
+                        )
+                    },
+                )
+                Button(
+                    onClick = {
+                        if(code.length!=6){
+                            Toast.makeText(context,"Invalid Code",Toast.LENGTH_SHORT).show()
+                        }
+                    },
+                    colors =  ButtonDefaults.buttonColors(
+                        containerColor = Primary,
+                        contentColor = Color.Black
+                    ),
+                    modifier = Modifier.weight(1.5f)
+                        .padding(vertical = 4.dp)
+                ) {
+                    Text(
+                        text = "Receive",
+                        style = Typography.bodyLarge,
+                    )
+                }
+            }
+        }
+
+    }
 }
 
 @Preview
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     FileLeapTheme {
-        Greeting("Android")
+        MainScreen()
     }
-
-}*/
+}
--- app/src/main/java/com/example/fileleap/ui/theme/Type.kt
+++ app/src/main/java/com/example/fileleap/ui/theme/Type.kt
@@ -9,35 +9,49 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.fileleap.R
 
-val clashGroTesk = FontFamily(
+val clashGrotesk = FontFamily(
     Font(R.font.clashgrotesk_bold, FontWeight.Bold),
-    Font(R.font.clashgrotesk_regular)
+    Font(R.font.clashgrotesk_semibold, FontWeight.SemiBold),
+    Font(R.font.clashgrotesk_medium, FontWeight.Medium),
+    Font(R.font.clashgrotesk_regular, FontWeight.Normal),
+    Font(R.font.clashgrotesk_light, FontWeight.Light),
+    Font(R.font.clashgrotesk_extralight, FontWeight.ExtraLight),
 )
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = clashGroTesk,
+        fontFamily = clashGrotesk,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-        color = Color.White
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = clashGrotesk,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = clashGrotesk,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = clashGrotesk,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleMedium = TextStyle(
+        fontFamily = clashGrotesk,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
